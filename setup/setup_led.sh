f48f42a0dfc6d833602c98ae032661d9f5218a33
Brian Machado <bmachado@live.ca>
2025-04-02 11:05:50 -0400

    Update setup_led.sh

--- setup/setup_led.sh
+++ setup/setup_led.sh
@@ -12,16 +12,13 @@ sudo apt-get install -y python3-spidev
 # Enable SPI interface
 sudo raspi-config nonint do_spi 0
 
-# Update pip
-pip3 install --upgrade pip
-
 # Install required Python packages
-pip3 install spidev
+python3 -m pip install Pi5Neo spidev
 
 echo "Applying patch to Pi5Neo library..."
 
 # Find pi5neo.py in the virtual environment and replace time.sleep(0.1) with time.sleep(0.01)
-PI5NEO_PATH=$(find .venv/lib -type f -name 'pi5neo.py')
+PI5NEO_PATH=$(find ~/ -type f -name 'pi5neo.py' 2>/dev/null | grep -E '/(\.venv|venv)/lib')
 
 if [ -f "$PI5NEO_PATH" ]; then
     sed -i 's/time\.sleep(0\.1)/time.sleep(0.01)/g' "$PI5NEO_PATH"
